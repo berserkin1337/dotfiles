24d82511606a501114a6b484ca4564da1e63ac9a
Aviral Gupta <gaviral373@gmail.com>
2023-12-16 06:21:04 +0530

    Initial commit

---
+++ dot_config/fish/conf.d/fnm.fish
@@ -0,0 +1,6 @@
+# fnm
+set PATH "/home/aviral/.local/share/fnm" $PATH
+fnm env | source
+# fnm
+set PATH "/home/aviral/.local/share/fnm" $PATH
+fnm env | source
---
+++ dot_config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias tmux="tmux -u"
+alias v="nvim"
+fish_add_path ~/.local/bin/
+fish_add_path ~/.cargo/bin/
+fish_add_path ~/.local/share/bob/nvim-bin
