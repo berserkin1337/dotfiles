14f05a0f9ad62eb6ec2c44a028b981124596ea6b
Aviral Gupta <gaviral373@gmail.com>
2024-01-07 18:40:41 +0530

    made changes

---
+++ dot_config/fish/conf.d/kdesrc-build.fish
@@ -0,0 +1,6 @@
+
+# kdesrc-build #################################################################
+
+## Add kdesrc-build to PATH
+fish_add_path --global /home/aviral/kde/src/kdesrc-build
+
--- dot_config/nvim/lua/plugins/example.lua
+++ dot_config/nvim/lua/plugins/example.lua
@@ -1,6 +1,4 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
--- if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -9,7 +7,6 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- Configure LazyVim to load gruvbox
 
   -- change trouble config
   {
